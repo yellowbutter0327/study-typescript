f484e064da3d8973f7b6e662bb1c45e61e282472
yellowbutter0327 <slrkwhdmsp@naver.com>
2023-07-02 15:18:25 +0900

    코딩애플 part 1 / 기본 타입 정리 (primitive types)

--- practice.js
+++ practice.js
@@ -1,14 +1,28 @@
-var 이름 = 123;
-//number,boolean, undefined,null까지 가능
+//1. string, number, undefined, null 타입도 있다.
+var 이름 = "으네";
 var 나이 = 24;
 var 결혼했니 = true;
 var 다이어트하니 = undefined;
-
-//array의 경우 []로 타입지정한다.
-var 회원들 = ["kim", "park"];
-
-//object형도 가능하다.
-var 회원들 = {
+// 2. array의 경우 []로 타입지정한다.
+var 회원들 = ["kim", "park"]; // 0이라고 기입하면 에러가 나겠죠.
+//3. object형도 가능하다.
+var 고객들 = {
     member1: "kim",
     member2: "park",
+};
+var project = {
+    member: ["kim", "park"],
+    days: 30,
+    started: true,
+};
+// 4. 타입스크립트에서 타입 지정 원래 자동으로 됩니다. 문법 생략 가능.
+// 5. 예지 문제
+var favorite = {
+    song: "곁에 있어줘",
+    singer: "sole",
+};
+var test = {
+    member: ["kim", "park"],
+    days: 30,
+    started: true,
 };
--- practice.ts
+++ practice.ts
@@ -1,26 +1,38 @@
-type test = string | number;
-let 이름: test = 123;
-
-//number,boolean, undefined,null까지 가능
+//1. string, number, undefined, null 타입도 있다.
+let 이름: string = "으네";
 let 나이: number = 24;
 let 결혼했니: boolean = true;
 let 다이어트하니: undefined = undefined;
 
-//array의 경우 []로 타입지정한다.
-let 회원들: string[] = ["kim", "park"];
+// 2. array의 경우 []로 타입지정한다.
+let 회원들: string[] = ["kim", "park"]; // 0이라고 기입하면 에러가 나겠죠.
 
-//object형도 가능하다.
+//3. object형도 가능하다.
 let 고객들: { member1: string; member2: string } = {
   member1: "kim",
   member2: "park",
 };
 
-let project :{
-    member : string[],
-    days : number,
-    started : boolean,
-  } = {
-    member : ['kim', 'park'],
-    days : 30,
-    started : true,
-  }
+let project = {
+  member: ["kim", "park"],
+  days: 30,
+  started: true,
+};
+
+// 4. 타입스크립트에서 타입 지정 원래 자동으로 됩니다. 문법 생략 가능.
+
+// 5. 예지 문제
+let favorite: { song: string; singer: string } = {
+  song: "곁에 있어줘",
+  singer: "sole",
+};
+
+let test: {
+  member: string[];
+  days: number;
+  started: boolean;
+} = {
+  member: ["kim", "park"],
+  days: 30,
+  started: true,
+};
