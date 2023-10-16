f6692e6344780e29458b2aa0118b9dd54b3c12a5
yellowbutter0327 <slrkwhdmsp@naver.com>
2023-10-16 23:59:26 +0900

    part2/literal type narrowing

--- practice.ts
+++ practice.ts
@@ -351,3 +351,21 @@ let 날짜 = new Date();
 if (날짜 instanceof Date) {
   console.log("참이에요");
 }
+
+//12.2 iteral type narrowing
+type Car = {
+  wheel: "4개";
+  color: string;
+};
+type Bike = {
+  wheel: "2개";
+  color: string;
+};
+
+function 함수(x: Car | Bike) {
+  if (x.wheel === "4개") {
+    console.log("the car is " + x.color);
+  } else {
+    console.log("the bike is " + x.color);
+  }
+}
